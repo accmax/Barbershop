8b4e2a3d7f545036f1b42be8663dd18bf7662ee4
Maksym Serebrennykov <73495553+accmax@users.noreply.github.com>
2022-09-27 18:28:11 +0300

    add submit form changes

--- js/menu-button.js
+++ js/menu-button.js
@@ -1,34 +1,16 @@
-// (() => {
-//   const openMenuBtnRef = document.querySelector("[data-menu-open]");
-//   const closeMenuBtnRef = document.querySelector("[data-menu-close]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-//   const backdropRef = document.querySelector('[data-backdrop]');
-//   const closeModalLink = document.querySelector('[burger-menu-close]');
-
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//     mobileMenuRef.classList.toggle("is-open");
-//     backdropRef.classList.toggle('is-hidden')
-//     closeModalLink.classList.toggle("is-open");
-//   });
-// })();
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-menu-close]'),
-     closeModalLink: document.querySelector('[burger-menu-close]'),
+    closeModalLink: document.querySelector('[burger-menu-close]'),
+    closeModalSecondLink: document.querySelector('[mobile-menu-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.closeModalLink.addEventListener('click', toggleModal);
+  refs.closeModalSecondLink.addEventListener('click', toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle('modal-open')
